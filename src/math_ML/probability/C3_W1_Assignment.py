e4f46e9112ccdb7411cc268a818827111c12c031
leonnn69 <leoneleazar23@gmail.com>
2023-10-02 00:47:50 +0700

    adding probability and course machine learning

---
+++ src/math_ML/probability/C3_W1_Assignment.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Define the size of the array
+array_size = 10
+
+# Create an array with random values in the range 1 to 10
+random_array = np.random.randn(1, 11, size=array_size)
+
+# Print the resulting array
+print(random_array)
